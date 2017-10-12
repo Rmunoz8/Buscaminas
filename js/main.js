34b34c89ab7f823d0a152b92f78776b199cb6d0f
Rubos8 <rmunoz@iesfuentesanluis.org>
2017-10-12 22:15:13 +0200

    -

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
 //     Ocultando elementos ocultos
     document.getElementById("liReiniciar").style.display = "none";
     document.getElementById("dificultad").style.display = "none" ;
+    document.getElementById("reglas").style.display = "none";
 
     let jugar = () =>{
 
+        document.getElementById("reglas").style.display = "none";
         document.getElementById("liJugar").style.display = "none";
         document.getElementById("titulo").style.display = "none";
         document.getElementById("liReiniciar").style.display = "block";
@@ -16,16 +18,25 @@
 
      let reiniciar = () =>{
 
+         document.getElementById("reglas").style.display = "none";
+         document.getElementById("dificultad").style.display = "none" ;
          document.getElementById("liReiniciar").style.display = "none";
          document.getElementById("liJugar").style.display = "block";
+         document.getElementById("titulo").style.display = "block";
+     }
+
+     let reglas = () =>{
+         document.getElementById("titulo").style.display = "none";
+         document.getElementById("dificultad").style.display = "none";
+         document.getElementById("reglas").style.display = "block";
      }
 
     var botonJugar = document.getElementById("botonJugar");
     var botonReinicar = document.getElementById("botonReinicar");
+    var botonReglas = document.getElementById("botonReglas");
 
     botonJugar.addEventListener("click", jugar, false);
     botonReinicar.addEventListener("click", reiniciar, false);
-
-
+    botonReglas.addEventListener("click", reglas, false);
 
 }
